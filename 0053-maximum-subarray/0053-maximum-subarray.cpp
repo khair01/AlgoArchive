7210f7a02f37829538273cde2176fa094fbabc05
Khair Muhammad <137539863+khair01@users.noreply.github.com>
2024-09-17 00:51:19 +0500

    Time: 99 ms (5.08%), Space: 70.5 MB (89.54%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,19 +1,43 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-
-        int maxarray = nums[0];
-        int res = nums[0];
-
-        // if(nums.size() == 1){
-        //     return nums[0];
-        // }
-        for(int i = 1 ; i < nums.size() ; i++){
-
-            maxarray = max(maxarray + nums[i], nums[i]);
-            res = max(maxarray,res);
+    int maxCrossingSum(vector<int>& nums, int low, int mid, int high) {
+        int leftSum = INT_MIN;
+        int sum = 0;
+        for (int i = mid; i >= low; i--) {
+            sum += nums[i];
+            if (sum > leftSum) {
+                leftSum = sum;
+            }
         }
-        return res;
-        
+
+        int rightSum = INT_MIN;
+        sum = 0;
+        for (int i = mid + 1; i <= high; i++) {
+            sum += nums[i];
+            if (sum > rightSum) {
+                rightSum = sum;
+            }
+        }
+
+        return leftSum + rightSum;
+    }
+
+    int maxSubArrayHelper(vector<int>& nums, int low, int high) {
+        if (low == high) {
+            return nums[low];
+        }
+
+        int mid = (low + high) / 2;
+
+        int leftSum = maxSubArrayHelper(nums, low, mid);
+        int rightSum = maxSubArrayHelper(nums, mid + 1, high);
+
+        int crossSum = maxCrossingSum(nums, low, mid, high);
+
+        return max(leftSum, max(rightSum, crossSum));
+    }
+
+    int maxSubArray(vector<int>& nums) {
+        return maxSubArrayHelper(nums, 0, nums.size() - 1);
     }
 };
