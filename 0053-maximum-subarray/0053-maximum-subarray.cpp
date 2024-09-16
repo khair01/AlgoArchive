240d66d1420a24f519e251060076997aba5260ad
Khair Muhammad <137539863+khair01@users.noreply.github.com>
2024-09-17 00:25:09 +0500

    Time: 80 ms (39.56%), Space: 70.5 MB (89.57%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,15 +4,15 @@ public:
 
         int maxarray = nums[0];
         int res = nums[0];
-        if(nums.size() == 1){
-            return nums[0];
-        }
+
+        // if(nums.size() == 1){
+        //     return nums[0];
+        // }
         for(int i = 1 ; i < nums.size() ; i++){
 
             maxarray = max(maxarray + nums[i], nums[i]);
             res = max(maxarray,res);
         }
-
         return res;
         
     }
